dcdca4356d6ad3222d7f4743abf9b774960aed64
strawhatrag <sreeragbharathan791@gmail.com>
2023-09-04 18:40:35 +0530

    fib

--- recurrsion.js
+++ recurrsion.js
@@ -4,3 +4,13 @@ function findFactorialRecursive(n) {
   }
   return n * findFactorialRecursive(n - 1);
 }
+
+function fibonacciRecursive(n) {
+  while (n < 2) {
+    return n;
+  }
+
+  return fibonacciRecursive(n - 1) + fibonacciIterative(n - 2);
+}
+
+console.log(fibonacciRecursive(10));
