10ee74638a67d9a5a9e982697c0bdf54c7c111b1
strawhatrag <sreeragbharathan791@gmail.com>
2023-09-14 21:20:11 +0530

    fib dynamic

--- dynamic programing/fib.js
+++ dynamic programing/fib.js
@@ -1,3 +1,6 @@
+const prompt = require("prompt-sync")();
+const input = prompt("enter fib number :");
+
 // 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, …
 
 function fibonacci(n) {
@@ -5,7 +8,21 @@ function fibonacci(n) {
 
   return fibonacci(n - 1) + fibonacci(n - 2);
 }
-const prompt = require("prompt-sync")();
-const input = prompt("enter fib number :");
 
-console.log("your ans fib num is :" + fibonacci(input));
+function fibFaster() {
+  let cache = {};
+
+  return function fib(n) {
+    if (n < 2) return n;
+
+    if (cache[n]) {
+      return cache[n];
+    } else {
+      cache[n] = fib(n - 1) + fib(n - 2);
+      return cache[n];
+    }
+  };
+}
+
+const calculateFib = fibFaster();
+console.log(calculateFib(input));
